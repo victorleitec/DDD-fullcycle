edbda8ac3f5f4e06f206b342d63f9f3f72085236
Victor Leite Costa <victorleitecosta@outlook.com.br>
2022-07-21 17:34:26 -0300

    Updated at 21/07/22

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,49 @@
+import Address from "./address";
+import Customer from "./customer";
+
+describe("Customer unit tests", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      let customer = new Customer("", "Fulano");
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      let customer = new Customer("123", "");
+    }).toThrowError("Name is required");
+  });
+
+  it("should change name", () => {
+    const customer = new Customer("123", "Fulano");
+    customer.changeName("Cicrano");
+    expect(customer.name).toBe("Cicrano");
+  });
+
+  it("should activate customer", () => {
+    const customer = new Customer("1", "Customer 1");
+    const address = new Address("Street 1", 123, "12345-678", "São Paulo");
+    customer.Address = address;
+
+    customer.activate();
+
+    expect(customer.isActive()).toBe(true);
+  });
+
+  it("should deactivate customer", () => {
+    const customer = new Customer("1", "Customer 1");
+    const address = new Address("Street 1", 123, "12345-678", "São Paulo");
+    customer.Address = address;
+
+    customer.deactivate();
+
+    expect(customer.isActive()).toBe(false);
+  });
+
+  it("should throw error when address is undefined when you activate a customer", () => {
+    expect(() => {
+      const customer = new Customer("1", "Customer 1");
+      customer.activate();
+    }).toThrowError("Address is mandatory to activate a customer");
+  });
+});
---
+++ src/entity/product.spec.ts
@@ -0,0 +1,33 @@
+import Product from "./product";
+
+describe("Product unit test", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      const product = new Product("", "Product 1", 100);
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      const product = new Product("p1", "", 100);
+    }).toThrowError("Name is required");
+  });
+
+  it("should throw error when price is less than 0", () => {
+    expect(() => {
+      const product = new Product("p1", "Product 1", -10);
+    }).toThrowError("Price must be greater than zero");
+  });
+
+  it("should change name", () => {
+    const product = new Product("p1", "Product 1", 100);
+    product.changeName("Product 2");
+    expect(product.name).toBe("Product 2");
+  });
+
+  it("should change price", () => {
+    const product = new Product("p1", "Product 1", 100);
+    product.changePrice(200);
+    expect(product.price).toBe(200);
+  });
+});
